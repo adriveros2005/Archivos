32b7337d72bbce1ec86558e44c896fda351ff09e
adriveros2005 <adriveros2005@hotmail.com>
2023-11-25 17:30:27 -0500

    inserta Mensaje aquí

--- app/src/main/java/com/example/archivos/MainActivity.java
+++ app/src/main/java/com/example/archivos/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 blector.close();
                 String archivoContenido = contenido.toString();
             }catch (IOException ex){
+                
                 ex.printStackTrace();
             }
         }
